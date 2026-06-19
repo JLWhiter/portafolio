b5f5d9f5d4214cd538588e4e7b45e2964c36a8e6
jose1801 <153047562+JLWhiter@users.noreply.github.com>
2026-06-19 15:42:39 -0500

    se agrego iconos para la vizualisacion de la direccion

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,8 @@
+import { LinkedIn, GitHub } from "@deemlol/next-icons"
 
 export default function Footer() {
   return (
-   <footer
+  <footer
     className="relative flex w-full bg-gray-900 text-white 
     items-center justify-center text-center top-[-20px] p-8 gap-12
     bg-gray-900 "
@@ -15,16 +16,24 @@ export default function Footer() {
       href="https://www.linkedin.com/in/jos%C3%A9-luis-chamorro-baldera-048489320/"
       target="_blank"
       rel="noopener noreferrer"
+      className="flex gap-2"
     >
-      LinkedIn
+      
+      <span>LinkedIn</span>
+
+      <LinkedIn size={20} className="inline-block ml-1" />
     </a>
 
     <a
       href="https://github.com/JLWhiter"
       target="_blank"
       rel="noopener noreferrer"
+      className=" flex gap-2"
     >
-      GitHub
+      <span>GitHub</span>
+  
+      <GitHub size={20} className="inline-block ml-1" />
+    
     </a>
   </div>
   </footer>
