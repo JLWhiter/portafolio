8f100f47c7b1d1068df98d57bc983065be4c61f5
jose1801 <153047562+JLWhiter@users.noreply.github.com>
2026-06-19 23:04:35 -0500

    se agrego el maquetado

---
+++ app/components/panel.tsx
@@ -0,0 +1,9 @@
+
+export default function Panel() {
+    return (
+        <div className="panel">
+            <h2>Panel de Control</h2>
+            <p>Bienvenido al panel de control. Aquí puedes gestionar tus proyectos, ver estadísticas y configurar tus preferencias.</p>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,74 +1,87 @@
 import Footer from "./components/footer";
 import Proyectos from "./components/proyectos";
+import Panel from "./components/panel";
 
 export default function Home() {
   return (
     <main className="contenedor">
-      <section className="inicio">
-        <div data-aos="fade-right" className="texto-inicio" >
-          <p className="saludo">Hola, soy</p>
-          <h1>José Luis Chamorro Baldera</h1>
-          <h2>Estudiante de Ingeniería de Software</h2>
-          <p>
-            Me gusta el desarrollo web, las bases de datos y crear soluciones
-            digitales que sean útiles, modernas y fáciles de usar.
-          </p>
+      <div className="inside" >
+        <Panel />
+      </div>
+      <div className="content" >
 
-          <div className="botones">
-            <a href="#proyectos" className="boton-principal hvr-border-fade hvr-float-shadow" >
-              Ver proyectos
-            </a>
+        <section className="inicio">
 
-            <a href="#contacto" className="boton-secundario hvr-border-fade hvr-float-shadow">
-              Contacto
-            </a>
+
+          <div data-aos="fade-right" className="texto-inicio" >
+            <p className="saludo">Hola, soy</p>
+            <h1>José Luis Chamorro Baldera</h1>
+            <h2>Estudiante de Ingeniería de Software</h2>
+            <p>
+              Me gusta el desarrollo web, las bases de datos y crear soluciones
+              digitales que sean útiles, modernas y fáciles de usar.
+            </p>
+
+            <div className="botones">
+              <a href="#proyectos" className="boton-principal hvr-border-fade hvr-float-shadow" >
+                Ver proyectos
+              </a>
+
+              <a href="#contacto" className="boton-secundario hvr-border-fade hvr-float-shadow">
+                Contacto
+              </a>
+            </div>
+
           </div>
 
-        </div>
+          <div data-aos="fade-left" className="tarjeta-perfil " >
+            <h3 >Sobre mí</h3 >
+            <p className="hvr-border-fade hvr-float-shadow">6to ciclo</p>
+            <p className="hvr-border-fade hvr-float-shadow">UTP</p>
+            <p className="hvr-border-fade hvr-float-shadow">Frontend | Backend | BD</p>
+          </div>
 
-        <div data-aos="fade-left" className="tarjeta-perfil " >
-          <h3 >Sobre mí</h3 >
-          <p className="hvr-border-fade hvr-float-shadow">6to ciclo</p>
-          <p className="hvr-border-fade hvr-float-shadow">UTP</p>
-          <p className="hvr-border-fade hvr-float-shadow">Frontend | Backend | BD</p>
-        </div>
-      </section>
+        </section>
 
-      <section data-aos="fade-up" className="seccion" id="sobre-mi" >
-        <h2>Sobre mí</h2>
-        <p>
-          Soy estudiante de Ingeniería de Software. Me interesa aprender nuevas
-          tecnologías, mejorar mis habilidades de programación y desarrollar
-          proyectos que ayuden a resolver problemas reales.
-        </p>
-      </section>
+        <section data-aos="fade-up" className="seccion" id="sobre-mi" >
+          <h2>Sobre mí</h2>
+          <p>
+            Soy estudiante de Ingeniería de Software. Me interesa aprender nuevas
+            tecnologías, mejorar mis habilidades de programación y desarrollar
+            proyectos que ayuden a resolver problemas reales.
+          </p>
 
-      <section data-aos="fade-up" className="seccion">
-        <h2>Habilidades</h2>
-        <div className="grid">
-          <div className="card hvr-border-fade hvr-float-shadow">tailwind</div>
-          <div className="card hvr-border-fade hvr-float-shadow">JavaScript</div>
-          <div className="card hvr-border-fade hvr-float-shadow">React</div>
-          <div className="card hvr-border-fade hvr-float-shadow">Next.js</div>
-          <div className="card hvr-border-fade hvr-float-shadow">Java</div>
-          <div className="card hvr-border-fade hvr-float-shadow">PostgreSQL</div>
-          <div className="card">MongoDB</div>
-        </div>
-      </section>
+        </section>
 
-      <section data-aos="fade-up" className="seccion" id="proyectos">
-        <h2>Proyectos</h2>
-        <Proyectos />
-      </section>
+        <section data-aos="fade-up" className="seccion">
+          <h2>Habilidades</h2>
+          <div className="grid">
+            <div className="card hvr-border-fade hvr-float-shadow">tailwind</div>
+            <div className="card hvr-border-fade hvr-float-shadow">JavaScript</div>
+            <div className="card hvr-border-fade hvr-float-shadow">React</div>
+            <div className="card hvr-border-fade hvr-float-shadow">Next.js</div>
+            <div className="card hvr-border-fade hvr-float-shadow">Java</div>
+            <div className="card hvr-border-fade hvr-float-shadow">PostgreSQL</div>
+            <div className="card">MongoDB</div>
+          </div>
+        </section>
 
-      <section data-aos="fade-up" className="seccion" id="contacto">
-        <h2>Contacto</h2>
-        <p>Correo: josebaldera.2021@gmail.com</p>
-        <p>Ubicación: Lima, Perú</p>
-      </section>
+        <section data-aos="fade-up" className="seccion" id="proyectos">
+          <h2>Proyectos</h2>
+          <Proyectos />
+        </section>
 
-      <Footer/>
-    
+        <section data-aos="fade-up" className="seccion" id="contacto">
+          <h2>Contacto</h2>
+          <p>Correo: josebaldera.2021@gmail.com</p>
+          <p>Ubicación: Lima, Perú</p>
+        </section>
+      </div>
+
+    <div className="footer">
+      <Footer />
+    </div>
+
     </main>
   );
 }
